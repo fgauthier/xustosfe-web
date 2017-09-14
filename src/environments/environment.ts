39be057aec0d651c5b4e7bf91edff0836f8bfb4c
Francois Gauthier <francois.gauthier@ieee.org>
2017-09-13 17:24:31 -0700

    Initial checkin

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: false,
+
+    region: 'us-east-1',
+
+    identityPoolId: 'us-east-1:9e9d550f-5f53-475a-b38a-cd30efc8bf9a',
+    userPoolId: 'us-east-1_3bjiw6GWN',
+    clientId: 'rjuicbqosc64po7gkc4javo40',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
+
